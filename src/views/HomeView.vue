00a15d6e3cb5623486003efd8674bc3830b68462
Ivy-Ho <ivycodefive@gmail.com>
2024-06-10 14:59:26 +0800

    (test) change HomeView text color

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
-  <p v-if="userStore.isLoggedIn" class="text-center text-xl text-violet-500">Welcome!</p>
-  <p v-else class="text-center text-xl text-violet-500">Welcome! Please Login.</p>
+  <p v-if="userStore.isLoggedIn" class="text-center text-xl">Welcome!</p>
+  <p v-else class="text-center text-xl">Welcome! Please Login.</p>
 </template>
 <script setup lang="ts">
 import { useUserStore } from '@/stores/modules/user'
